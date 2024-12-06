1896a6e0a8a71a05fb3b9eab93b02c7d919f704e
MH <dico659@gmail.com>
2024-12-06 21:35:41 +0900

    feat: 상품목록페이지 Sidebar

--- frontend/src/routes/pages/ProductList.tsx
+++ frontend/src/routes/pages/ProductList.tsx
@@ -5,12 +5,36 @@ function ProductList() {
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
   ];
 
+  const sideMenuArray = [
+    {
+      text: "one-piece",
+    },
+    {
+      text: "full-body",
+    },
+  ];
+
   return (
-    <div className="flex">
-      <div className="mr-20 grid w-[1064px] grid-cols-4 gap-[19px]">
-        {productDummyArray.map((product, i) => (
-          <ProductItem key={i} /> // 데이터 불러오면 [product.id로]
-        ))}
+    <div>
+      <div className="ml-20 mr-20 flex">
+        <ul>
+          {sideMenuArray.map((sideMenu) => (
+            <li key={sideMenu.text} className="px-[33px] py-[10px]">
+              <button className="peer text-nowrap text-[26px] font-medium first:cursor-pointer hover:font-semibold focus:font-semibold">
+                {sideMenu.text}
+              </button>
+              <img
+                className="invisible peer-hover:visible peer-focus:visible"
+                src="/public/images/underline-menu.png"
+              />
+            </li>
+          ))}
+        </ul>
+        <div className="tablet:grid-cols-3 desktop:grid-cols-5 grid w-full grid-cols-1 gap-[19px]">
+          {productDummyArray.map((product, i) => (
+            <ProductItem key={i} /> // 데이터 불러오면 [product.id로]
+          ))}
+        </div>
       </div>
     </div>
   );
