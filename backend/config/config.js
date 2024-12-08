144cd67131ba7202797c6083ef2f7ad1898f2ce0
MH <dico659@gmail.com>
2024-12-08 21:00:18 +0900

    feat: config file and hide DB configs

---
+++ backend/config/config.js
@@ -0,0 +1,14 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const config = {
+  database: process.env.DB_DATABASE,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOSTNAME,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT,
+};
+
+module.exports = config;
