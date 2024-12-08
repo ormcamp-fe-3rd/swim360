da4967a9b4afba17cb3b2fa6da843b0a48e99a8a
MH <dico659@gmail.com>
2024-12-08 21:00:18 +0900

    feat: config file and hide DB configs

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const db = require("./db");
 const models = require("./models");
+const config = require("./config/config");
 
 const startServer = async () => {
   await db.init();
@@ -11,8 +12,8 @@ const startServer = async () => {
 
   app.use(require("./routes"));
 
-  app.listen(3000, () => {
-    console.log("server listening on port 3000");
+  app.listen(config.port, () => {
+    console.log(`server listening on port${config.port}`);
   });
 };
 
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,15 +3,73 @@ const router = express.Router();
 const { User } = require("../models");
 
 // 유저 전체 조회
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users).send();
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // 신규 유저 생성
-router.post("/", async (req, res, next) => {});
+router.post("/", async (req, res, next) => {
+  try {
+    const { name, nickname, age } = req.body;
+    // 요청 파라미터 검증
+    if (!name || !nickname || !age) {
+      next(Error(`missing required parameters`));
+    }
+
+    const newUser = await User.create({ name, nickname, age });
+    res.json(newUser).send();
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // 특정 유저 조회
-router.get("/:id", (req, res) => {});
+router.get("/:id", async (req, res) => {
+  try {
+    const userId = req.params.id;
+
+    const user = await User.findOne({ where: { id: userId } });
+    res.send("특정 ID에 대한 유저 조회:" + userId);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // 특정 유저 수정
-router.put("/:id", (req, res) => {});
+router.put("/:id", async (req, res) => {
+  try {
+    const { name, nickname, age, id } = req.body;
+    // 요청 파라미터 검증
+    if (!name || !nickname || !age) {
+      next(Error(`missing required parameters`));
+    }
+
+    // 유저 존재 여부 확인
+    const user = await User.findOne({ where: { id: id } });
+    if (!user) {
+      return res.send({ status: 404, message: "User not found" });
+    }
+
+    // 유저 있으면 수정
+    await User.update(
+      { name, nickname, age },
+      {
+        where: {
+          id: id,
+        },
+      }
+    );
+
+    const updatedUser = await User.findOne({ where: { id: id } });
+    res.send(updatedUser);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
