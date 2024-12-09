0e415ee3529c1da3b24cfcf5c2827fd49ac30a95
김혜빈 <hyebinkimdesign@gmail.com>
2024-12-10 01:06:50 +0900

    design: 팝업 2종 추가

--- frontend/src/components/product/reviewsTab.tsx
+++ frontend/src/components/product/reviewsTab.tsx
@@ -1,36 +1,14 @@
-import ReviewStar from "./reviewsStar";
-import ReviewPopup from "./ReviewPopup";
+import ReviewStar from "./ReviewsStar";
+import ReviewPopup from "./ReviewWriteModal";
+import ReviewDetailModal from "./ReviewDetailModal";
 
 import { PaginationDemo } from "../common/Paginaionm";
+import Reviews from "./Reviews";
 
-
-
-function Reviews(){
-    return(
-        <div className="w-[1064px] h-[196px] flex items-center border-b border-[#D9D9D9]">
-        <div className="h- flex-col gap-10">
-        <ReviewStar size="small"/>
-        <div className="w-[1064px] flex justify-between">
-        <div className="flex-col">
-        <div className="flex font-medium text-[18px]">
-        <p>사이즈:</p>
-        <p> 000 </p>
-        </div>
-        <p className="w-[920px] font-light">최근에 이 사각 수영복을 구매했는데, 정말 만족스럽습니다! 핏이 딱 좋아요—몸에 딱 맞으면서도 답답하지 않습니다. 소재는 내구성이 있으면서도 편안하고, 물놀이 후에도 금방 마르더라고요. 몇 번 입어봤는데도 색상이 전혀 바래지 않았습니다. 디자인도 세련돼서 정말 마음에 들어요. 강력 추천합니다!</p>
-        </div>
-        <img className="w-[120px] h-[120px]"></img>
-        </div>
-        <div className="flex justify-start gap-2 text-[#B0B0B0] text-[12px] font-medium">
-        <p >김** </p>
-        <p > | 2000.00.00  </p>
-        </div>
-        </div>       
-        </div>
-    )
-}
 
 
 function ReviewsTab(){
+  
     return(
         <div className="w-[1064px] mx-auto">
 
@@ -51,9 +29,13 @@ function ReviewsTab(){
           <button id="ReviewButton" className=" bg-black w-[164px] h-[50px] rounded-xl text-white text-[24px] flex items-center justify-center gap-2 "><img src="/public/images/icon-reviews.png"/>후기 등록</button>
           </div>
           
+          <div className="hidden">
           <ReviewPopup/>
-         
+          </div>
 
+          <div>
+          <ReviewDetailModal/>
+          </div>
           <Reviews/>
           <Reviews/>
           <Reviews/>
