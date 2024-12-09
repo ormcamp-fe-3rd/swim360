ede3c24365327f660c5540c9712c8dadc5dbd60c
MH <dico659@gmail.com>
2024-12-10 01:55:40 +0900

    fix: dummydata 전달

--- frontend/src/routes/pages/MyPage.tsx
+++ frontend/src/routes/pages/MyPage.tsx
@@ -4,22 +4,91 @@ import UserInfoPreview from "@/components/mypage/UserInfoPreview";
 import UserPointAndReviewPreview from "@/components/mypage/UserPointAndReviewPreview";
 
 function MyPage() {
+  const userDummyData = {
+    user: {
+      name: "먕먕",
+    },
+    orderStatsCount: {
+      ORDER_COMPLETE: 12,
+      PAYMENT_COMPLETE: 22,
+      PREPARING: 12,
+      SHIPPING: 15,
+      DELIVERED: 11,
+    },
+    orders: [
+      {
+        id: 2020301023102,
+        createdAt: "2024-12-10T10:30:15.000Z",
+        price: 50000,
+        orderItems: [
+          {
+            id: 0,
+            name: "best 강습용 여자 수영복",
+            size: "M",
+            imageUrl: "/public/images/mypage/sample-1.png",
+            quantity: 2,
+            discountedPrice: 30000,
+          },
+          {
+            id: 1,
+            name: "best 강습용 남자 수영복",
+            size: "XL",
+            imageUrl: "/public/images/mypage/sample-3.png",
+            quantity: 1,
+            discountedPrice: 30000,
+          },
+          {
+            id: 2,
+            name: "best 강습용 어린이 수영복",
+            size: "S",
+            imageUrl: "/public/images/mypage/sample-2.png",
+            quantity: 1,
+            discountedPrice: 10000,
+          },
+        ],
+      },
+      {
+        id: 20203033333,
+        createdAt: "2024-12-09T10:30:15.000Z",
+        price: 50000,
+        orderItems: [
+          {
+            id: 0,
+            name: "best 강습용 여자 수영복2",
+            size: "M",
+            imageUrl: "/public/images/mypage/sample-1.png",
+            quantity: 2,
+            discountedPrice: 30000,
+          },
+          {
+            id: 1,
+            name: "best 강습용 남자 수영복2",
+            size: "XL",
+            imageUrl: "/public/images/mypage/sample-3.png",
+            quantity: 1,
+            discountedPrice: 30000,
+          },
+          {
+            id: 2,
+            name: "best 강습용 어린이 수영복2",
+            size: "S",
+            imageUrl: "/public/images/mypage/sample-2.png",
+            quantity: 1,
+            discountedPrice: 10000,
+          },
+        ],
+      },
+    ],
+  };
+
   return (
     <div className="px-[82px]">
       <div className="mb-[69px] grid grid-cols-2 gap-[41px]">
-        <UserInfoPreview name={"먕먕"} />
+        <UserInfoPreview name={userDummyData.user.name} />
         <UserPointAndReviewPreview reviewCount={3} />
       </div>
-      <OrderStatusPreview
-        deliveryStateCounts={{
-          ORDER_COMPLETE: 12,
-          PAYMENT_COMPLETE: 22,
-          PREPARING: 12,
-          SHIPPING: 15,
-          DELIVERED: 11,
-        }}
-      />
-      <RecentOrderPreview />
+      <OrderStatusPreview deliveryStateCounts={userDummyData.orderStatsCount} />
+      <RecentOrderPreview orders={userDummyData.orders} />
     </div>
   );
 }
