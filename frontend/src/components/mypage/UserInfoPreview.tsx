15f2f552f1d0bc299df979fcd9bc4c339756067e
MH <dico659@gmail.com>
2024-12-09 16:38:15 +0900

    feat: UserInfoPreview layout

--- frontend/src/components/mypage/UserInfoPreview.tsx
+++ frontend/src/components/mypage/UserInfoPreview.tsx
@@ -1,5 +1,22 @@
-function UserInfoPreview() {
-  return <></>;
+import { Link } from "react-router-dom";
+
+interface UserInfoPreviewProps {
+  name: string;
+}
+
+function UserInfoPreview({ name }: UserInfoPreviewProps) {
+  return (
+    <div className="flex h-[146px] items-center justify-between border-[1px] border-[#000000] px-5">
+      <div className="text-xl font-semibold">{name}</div>
+      <Link to="/user" role="link">
+        <img
+          className="h-5 w-5"
+          src="/public/images/icon-mypage-user.png"
+          alt="내 정보 수정 페이지로 이동"
+        />
+      </Link>
+    </div>
+  );
 }
 
 export default UserInfoPreview;
