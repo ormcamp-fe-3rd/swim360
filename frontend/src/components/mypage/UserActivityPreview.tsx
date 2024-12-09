fab5cb85eaa4b2a1421870faae939ad906121106
MH <dico659@gmail.com>
2024-12-09 16:42:51 +0900

    feat: UserActivityPreview layout

--- frontend/src/components/mypage/UserActivityPreview.tsx
+++ frontend/src/components/mypage/UserActivityPreview.tsx
@@ -1,5 +1,25 @@
-function UserActivityPreview() {
-  return <></>;
+import { Link } from "react-router-dom";
+
+interface UserActivityPreviewProps {
+  reviewCount: number;
+}
+
+function UserActivityPreview({ reviewCount }: UserActivityPreviewProps) {
+  return (
+    <div className="flex h-[146px] w-full items-center justify-center border-[1px] border-[#000000] py-[30px]">
+      <div className="flex w-full flex-col items-center text-xl font-semibold">
+        <div className="mb-11">포인트</div>
+        <div>150p</div>
+      </div>
+      <Link
+        to="/reviews"
+        className="flex w-full flex-col items-center text-xl font-semibold"
+      >
+        <div className="mb-11">리뷰</div>
+        <div>{reviewCount}건</div>
+      </Link>
+    </div>
+  );
 }
 
 export default UserActivityPreview;
