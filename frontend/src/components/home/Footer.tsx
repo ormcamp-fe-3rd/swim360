7da449a2647246ed95a123a62b75cd3f47060961
ban <summer.jyyy@gmail.com>
2024-12-10 00:11:48 +0900

    design: 푸터 반응형 적용

--- frontend/src/components/home/Footer.tsx
+++ frontend/src/components/home/Footer.tsx
@@ -1,21 +1,21 @@
 // const footerImg =  "./public/images/common/footer.png";
 const footerClass =
-  "w-screen h-[510px]  bg-[url('/images/common/footer.png')] bg-cover flex flex-col justify-end items-center gap-[100px]";
+  "w-screen h-[510px] pt-10 bg-[url('/images/common/footer.png')] bg-cover flex flex-col justify-around items-center";
 
 export default function Footer(){
   return (
     <footer>
-      <div className="pt-[268px] h-96 w-full bg-white">
+      <div className="h-96 w-full bg-white pt-[268px]">
         <div className={footerClass}>
-          <ul className="flex gap-14">
+          <ul className="flex w-full max-w-[565px] justify-between px-10">
             <li className="font-semibold">회사소개</li>
             <li className="font-semibold">이용약관</li>
             <li className="font-semibold">개인정보처리방침</li>
           </ul>
-          <div className="mb-10 flex w-[565px] justify-between">
+          <div className="mb-10 flex w-full justify-between px-10 tablet:w-[565px]">
             <div>
               <p className="font-semibold">CALL CENTER</p>
-              <p>
+              <p className="text-xs tablet:text-base">
                 <br />
                 고객 센터: 000-000-0000 <br />
                 문자/카톡: 000-000-0000 <br />
@@ -27,7 +27,7 @@ export default function Footer(){
             </div>
             <div>
               <p className="font-semibold">BANK INFO</p>
-              <p>
+              <p className="text-xs tablet:text-base">
                 <br />
                 신한:000-000-000000 <br />
                 농협:000-00000-00000 <br />
