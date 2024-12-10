8cd7e52e51f5561935609e44bb0383db541a9aec
ban <summer.jyyy@gmail.com>
2024-12-10 15:15:45 +0900

    design: 카테고리 호버 추가

--- frontend/src/components/home/CategoryBox.tsx
+++ frontend/src/components/home/CategoryBox.tsx
@@ -18,44 +18,102 @@ const categoryNames: Category[] = [
     name: "ACC",
     productId: 3,
   },
-  // {
-  //   id: 4,
-  //   name: "원피스",
-  //   productId: 4,
-  //   parentId: 1
-  // },
-  // {
-  //   id: 5,
-  //   name: "반전신",
-  //   productId: 5,
-  //   parentId: 1
-  // },
-  // {
-  //   id: 6,
-  //   name: "5부",
-  //   productId: 6,
-  //   parentId: 2
-  // },
-  // {
-  //   id: 7,
-  //   name: "사각",
-  //   productId: 7,
-  //   parentId: 2
-  // },
-  // {
-  //   id: 8,
-  //   name: "수모",
-  //   productId: 8,
-  //   parentId: 3
-  // }
 ];
 
 export default function CategoryBox() {
   return (
-    <ul className=" gap-[52px] hidden tablet:flex">
-      {categoryNames.map((category) => (
-        <CategoryList key={category.id} category={category} />
-      ))}
-    </ul>
+    <>
+      <ul className="hidden gap-28 tablet:flex">
+        <li className="group text-3xl font-bold">
+          <a href="/productlist" className="flex justify-center">
+            WOMAN
+          </a>
+          <img
+            className="invisible pr-6 group-hover:visible group-focus:visible pl-5"
+            src="/public/images/productlist/underline-menu.png"
+          />
+          <div className="absolute mt-2 flex h-[142px] items-center gap-5 rounded-xl bg-white px-4 py-3 opacity-0 shadow-lg group-hover:opacity-100">
+            <a href="/productlist/onepiece">
+              <img
+                src="/images/home/icon-onepiece.png"
+                alt="원피스"
+                className="rounded-xl hover:bg-gray-100"
+              />
+              <p className="pt-2 text-center text-xs font-normal">one-piece</p>
+            </a>
+            <div className="h-16 w-[1px] bg-gray-200"></div>
+            <a href="/productlist/fullbody">
+              <img
+                src="/images/home/icon-fullbody.png"
+                alt="반전신"
+                className="rounded-xl hover:bg-gray-100"
+              />
+              <p className="pt-2 text-center text-xs font-normal">full-body</p>
+            </a>
+          </div>
+        </li>
+        <li className="group text-3xl font-bold">
+          <a href="/productlist" className="flex justify-center">MAN</a>
+          <img
+            className="invisible pr-6 group-hover:visible group-focus:visible pl-5"
+            src="/public/images/productlist/underline-menu.png"
+          />
+          <div className="absolute mt-2 flex h-[142px] items-center gap-5 rounded-xl bg-white px-4 py-3 opacity-0 shadow-lg group-hover:opacity-100">
+            <a href="/productlist/onepiece">
+              <img
+                src="/images/home/icon-midlength.png"
+                alt="원피스"
+                className="rounded-xl hover:bg-gray-100"
+              />
+              <p className="pt-2 text-center text-xs font-normal">mid-length</p>
+            </a>
+            <div className="h-16 w-[1px] bg-gray-200"></div>
+            <a href="/productlist/fullbody">
+              <img
+                src="/images/home/icon-squarecut.png"
+                alt="반전신"
+                className="rounded-xl hover:bg-gray-100"
+              />
+              <p className="pt-2 text-center text-xs font-normal">square-cut</p>
+            </a>
+          </div>
+        </li>
+        <li className="group text-3xl font-bold">
+          <a href="/productlist" className="flex justify-center">ACC</a>
+          <img
+            className="invisible pr-6 group-hover:visible group-focus:visible pl-5"
+            src="/public/images/productlist/underline-menu.png"
+          />
+          <div className="absolute mt-2 flex h-[142px] items-center gap-5 rounded-xl bg-white px-4 py-3 opacity-0 shadow-lg group-hover:opacity-100">
+            <a href="/productlist/onepiece">
+              <img
+                src="/images/home/icon-bag.png"
+                alt="원피스"
+                className="rounded-xl hover:bg-gray-100"
+              />
+              <p className="pt-2 text-center text-xs font-normal">bag</p>
+            </a>
+            <div className="h-16 w-[1px] bg-gray-200"></div>
+            <a href="/productlist/fullbody">
+              <img
+                src="/images/home/icon-towel.png"
+                alt="반전신"
+                className="rounded-xl hover:bg-gray-100"
+              />
+              <p className="pt-2 text-center text-xs font-normal">towel</p>
+            </a>
+            <div className="h-16 w-[1px] bg-gray-200"></div>
+            <a href="/productlist/fullbody">
+              <img
+                src="/images/home/icon-fin.png"
+                alt="반전신"
+                className="rounded-xl hover:bg-gray-100"
+              />
+              <p className="pt-2 text-center text-xs font-normal">fins</p>
+            </a>
+          </div>
+        </li>
+      </ul>
+    </>
   );
 }
