0cc89a6df73dca41f9d868ff74ce3b35f12a06ba
ban <summer.jyyy@gmail.com>
2024-12-12 11:32:34 +0900

    fix: 헤더, 푸터 common 폴더로 이동

---
+++ frontend/src/components/common/Header.tsx
@@ -0,0 +1,21 @@
+import CategoryBox from "../home/CategoryBox";
+import HamburgerMenu from "../home/HamburgerMenu";
+import MainIconButton from "../home/MainIconButton";
+import MainLogo from "../home/MainLogo";
+import ScrollButton from "../home/ScrollButton";
+
+function Header() {
+  return (
+    <header className="flex justify-center">
+      <nav className="mt-6 flex h-[190px] w-full items-center justify-between px-10 desktop:w-[1440px]">
+        <HamburgerMenu />
+        <MainLogo />
+        <CategoryBox />
+        <MainIconButton />
+      </nav>
+      <ScrollButton />
+    </header>
+  );
+}
+
+export default Header;
