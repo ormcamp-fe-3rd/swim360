1ffe7dd6763fe63310625cfef5ddadafca1ca3a1
ban <summer.jyyy@gmail.com>
2024-12-12 14:02:20 +0900

    fix: 헤더, 푸터 연결링크 수정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,8 @@ import "./styles/index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import Footer from "./components/home/Footer";
-import Header from "./components/home/Header";
+import Footer from "./components/common/Footer";
+import Header from "./components/common/Header";
 import Router from "./routes";
 
 createRoot(document.getElementById("root")!).render(
