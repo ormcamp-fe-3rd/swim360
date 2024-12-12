22abdc74412e4bc365ae624ed4c7584d0e41b538
ban <summer.jyyy@gmail.com>
2024-12-12 20:50:47 +0900

    design: 햄버거메뉴 backdrop 추가

--- frontend/src/components/common/Header.tsx
+++ frontend/src/components/common/Header.tsx
@@ -7,7 +7,7 @@ import ScrollButton from "../home/ScrollButton";
 function Header() {
   return (
     <header className="flex justify-center">
-      <nav className="mt-6 flex h-[190px] w-full items-center justify-between px-10 desktop:w-[1440px]">
+      <nav className="pt-6 flex h-[190px] w-full items-center justify-between px-10 desktop:w-[1440px]">
         <HamburgerMenu />
         <MainLogo />
         <CategoryBox />
--- frontend/src/components/home/HamburgerMenu.tsx
+++ frontend/src/components/home/HamburgerMenu.tsx
@@ -1,31 +1,34 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function HamburgerMenu(){
-  const[isOpen, setIsOpen] = useState(false)
-  
+
+
+export default function HamburgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="relative tablet:hidden">
       <button onClick={() => setIsOpen(!isOpen)}>
         <img
           className="h-6 w-6"
           src="/images/home/button-hamburger.png"
-          alt=""
-        />
+          alt="햄버거 메뉴"
+          />
       </button>
-      {/* TODO: backdrop 효과 추가 */}
-      <div className={`absolute ${isOpen ? "block" : "hidden"} -translate-x-10 top-20 z-10`}>
-        <div className="h-[370px] w-screen bg-white">
-          <ul className="flex h-full flex-col items-center">
+      <div
+        className={`${isOpen ? "block" : "hidden"} fixed top-[190px] z-10 inset-0 backdrop-brightness-50`}
+      >
+        <div className="h-full w-full">
+          <ul className="flex h-[420px] flex-col items-center bg-white">
             <li className="flex h-1/4 w-full items-center justify-center font-semibold shadow-sm">
               WOMAN
             </li>
-            <li className="flex h-1/4 w-full items-center justify-center font-semibold">
+            <li className="flex h-1/4 w-full items-center justify-center font-semibold shadow-sm">
               MAN
             </li>
-            <li className="flex h-1/4 w-full items-center justify-center font-semibold">
+            <li className="flex h-1/4 w-full items-center justify-center font-semibold shadow-sm">
               ACC
             </li>
-            <li className="flex h-1/4 w-full items-center justify-center font-semibold">
+            <li className="flex h-1/4 w-full items-center justify-center font-semibold shadow-sm">
               MY PAGE
             </li>
           </ul>
