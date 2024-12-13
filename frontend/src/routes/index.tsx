579c18e96a0527a257bf23ec1158dcc97e9ef531
ban <summer.jyyy@gmail.com>
2024-12-13 12:53:12 +0900

    feat: 라우터 path 추가

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/productlist", element: <ProductListPage /> },
+      { path: "/product_list/:id", element: <ProductListPage /> },
       {
         path: "/product/:id",
         element: <ProductDetailPage />,
