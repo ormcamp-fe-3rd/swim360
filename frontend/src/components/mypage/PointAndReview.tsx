90c9a8dbd84b22af0b63b47e7d4dcc6f39357e0d
ban <summer.jyyy@gmail.com>
2024-12-14 20:25:02 +0900

    cn 유틸리티 적용, 삼항연산자 수정

--- frontend/src/components/mypage/PointAndReview.tsx
+++ frontend/src/components/mypage/PointAndReview.tsx
@@ -1,29 +1,31 @@
 import { useState } from "react";
 
+import { cn } from "@/lib/utils";
+
 import { Tabs, TabsContent, TabsListCustom, TabsTriggerCustom } from "../ui/tabs";
 import PointList from "./PointList";
 import ReviewList from "./ReviewList";
 
 
 export default function PointAndReviewTab() {
-  const [tab, setTab] = useState("point")
+  const [tab, setTab] = useState(true);
 
   return (
     <div className="h-screen">
       <div className="my-10 w-full">
         <Tabs defaultValue="my-points" className="h-20">
-          <TabsListCustom className="h-full w-full tablet:w-[600px] mx-auto">
+          <TabsListCustom className="mx-auto h-full w-full tablet:w-[600px]">
             <TabsTriggerCustom
               value="my-points"
-              className={`h-full w-1/2 ${tab === "point" ? "border-black" : ""}`}
-              onClick={() => setTab("point")}
+              className={cn("h-full w-1/2", { "border-black": tab})}
+              onClick={() => setTab(true)}
             >
               포인트 내역
             </TabsTriggerCustom>
             <TabsTriggerCustom
               value="my-reviews"
-              className={`h-full w-1/2 ${tab === "review" ? "border-black" : ""}`}
-              onClick={() => setTab("review")}
+              className={cn("h-full w-1/2", { "border-black": !tab })}
+              onClick={() => setTab(false)}
             >
               리뷰 내역
             </TabsTriggerCustom>
