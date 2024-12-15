97a9f7afe76c522e1b4a0e1493f4a5341f01ddd8
MH <dico659@gmail.com>
2024-12-15 21:29:47 +0900

    fix: 목데이터 json파일로 분리, props 타입 외 타입은 types 폴더로 분, select에 데이터 연결

--- frontend/src/routes/pages/ProductListPage.tsx
+++ frontend/src/routes/pages/ProductListPage.tsx
@@ -1,15 +1,31 @@
 import ProductItemList from "@/components/product-list/ProductItemList";
 import SideBar from "@/components/product-list/Sidebar";
 import SortSelect from "@/components/product-list/SortSelect";
+import { SortOption } from "@/types/products";
+import { useState } from "react";
 
 function ProductListPage() {
+  const [sortOption, setsortOption] = useState<SortOption>("latest");
+
+  const onSortOptionChange = (sortOption: SortOption) => {
+    setsortOption(sortOption);
+  };
+
+  const sortSelectProps = {
+    onSortOptionChange,
+  };
+
+  const productItemListProps = {
+    sortOption,
+  };
+
   return (
     <div className="p-4">
       <div className="flex flex-col justify-center tablet:flex-row">
         <SideBar />
         <div>
-          <SortSelect />
-          <ProductItemList />
+          <SortSelect {...sortSelectProps} />
+          <ProductItemList {...productItemListProps} />
         </div>
       </div>
     </div>
