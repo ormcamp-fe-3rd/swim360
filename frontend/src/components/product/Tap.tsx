203b2db410b764824c770cbfa5285bfac59df9cd
김혜빈 <hyebinkimdesign@gmail.com>
2024-12-17 16:31:53 +0900

    fix: a태그 linkhash로 수정

--- frontend/src/components/product/Tap.tsx
+++ frontend/src/components/product/Tap.tsx
@@ -1,3 +1,5 @@
+import { HashLink } from "react-router-hash-link";
+
 interface ProductTabProps {
   activeCategory?: string; // 선택적으로 activeCategory를 받을 수 있도록 설정
 }
@@ -6,8 +8,9 @@ function ProductTab({ activeCategory = "" }: ProductTabProps) {
   return (
     <div className="scr mx-auto mb-8 mt-16 flex max-h-[44px] w-full justify-center">
       {/* 상품 상세 정보 */}
-      <a
-        href="#detailsTab"
+      <HashLink
+        smooth
+        to="#detailsTab"
         className={`flex h-[44px] w-[266px] flex-wrap items-center justify-center border-b-2 ${
           activeCategory === "detailsTab"
             ? "border-black text-black"
@@ -15,11 +18,12 @@ function ProductTab({ activeCategory = "" }: ProductTabProps) {
         } text-base font-medium sm:text-[24px]`}
       >
         상품상세정보
-      </a>
+      </HashLink>
 
       {/* 상품 후기 */}
-      <a
-        href="#reviewsTab"
+      <HashLink
+        smooth
+        to="#reviewsTab"
         className={`flex h-[44px] w-[266px] flex-wrap items-center justify-center border-b-2 ${
           activeCategory === "reviewsTab"
             ? "border-black text-black"
@@ -27,11 +31,12 @@ function ProductTab({ activeCategory = "" }: ProductTabProps) {
         } text-sm font-extralight sm:text-[24px]`}
       >
         상품후기
-      </a>
+      </HashLink>
 
       {/* 자주하는 질문 */}
-      <a
-        href="#qnasTab"
+      <HashLink
+        smooth
+        to="#qnasTab"
         className={`flex h-[44px] w-[266px] flex-wrap items-center justify-center border-b-2 ${
           activeCategory === "qnasTab"
             ? "border-black text-black"
@@ -39,11 +44,12 @@ function ProductTab({ activeCategory = "" }: ProductTabProps) {
         } text-sm font-extralight sm:text-[24px]`}
       >
         자주하는 질문
-      </a>
+      </HashLink>
 
       {/* 교환/반품 */}
-      <a
-        href="#exchangeTab"
+      <HashLink
+        smooth
+        to="#exchangeTab"
         className={`flex h-[44px] w-[266px] flex-wrap items-center justify-center border-b-2 ${
           activeCategory === "exchangeTab"
             ? "border-black text-black"
@@ -51,7 +57,7 @@ function ProductTab({ activeCategory = "" }: ProductTabProps) {
         } text-sm font-extralight sm:text-[24px]`}
       >
         교환/반품
-      </a>
+      </HashLink>
     </div>
   );
 }
