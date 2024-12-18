b62202ae51cea8126977d0c52108212246ad0d2c
MH <dico659@gmail.com>
2024-12-19 05:19:10 +0900

    feat: 상품 목록 불러오기 기능 구현

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,5 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const { Product } = require("../models");
+
+router.get("/:categoryId", async (req, res) => {
+  const { categoryId } = req.params;
+
+  try {
+    const products = await Product.findAll({
+      where: { category_id: categoryId },
+    });
+
+    if (!products) {
+      return res.json({ isProductsExist: false });
+    }
+
+    return res.json(products);
+  } catch (error) {
+    return res.status(500).json({ error: " 서버 에러 " + error });
+  }
+});
 
 router.get("/:id", (req, res) => {});
 
