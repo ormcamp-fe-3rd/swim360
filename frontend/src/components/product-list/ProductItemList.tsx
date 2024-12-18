9438f69c1733ecb0208c175b83e3efc165215053
MH <dico659@gmail.com>
2024-12-19 05:20:46 +0900

    fix: 상품 목록 페이지에 useProduct 훅 적용(상품 목록 불러오기, select 선택에 따른 필터링 기능)

--- frontend/src/components/product-list/ProductItemList.tsx
+++ frontend/src/components/product-list/ProductItemList.tsx
@@ -1,33 +1,15 @@
 import ProductItem from "@/components/product-list/ProductItem";
+import useProduct from "@/hooks/useProduct";
 
-import { Link } from "react-router-dom";
-
-import productsData from "@/mocks/products.json";
-import { ProductItemData, ProductsData, SortOption } from "@/types/products";
+// import productsData from "@/mocks/products.json";
+import { SortOption } from "@/types/products";
 
 interface ProductItemListProps {
   sortOption: SortOption;
 }
 
 function ProductItemList({ sortOption }: ProductItemListProps) {
-  const { products } = productsData as ProductsData; // mock 데이터
-
-  const sortFunctions = {
-    latest: (products: ProductItemData[]) =>
-      [...products].sort((a, b) => b.createdAt.localeCompare(a.createdAt)),
-
-    sale: (products: ProductItemData[]) =>
-      [...products].sort((a, b) => b.salesVolume - a.salesVolume),
-    review: (products: ProductItemData[]) =>
-      [...products].sort((a, b) => b.reviewCount - a.reviewCount),
-  };
-
-  const sortedProducts = (
-    products: ProductItemData[],
-    option: typeof sortOption,
-  ) => {
-    return sortFunctions[option](products);
-  };
+  const { products, sortedProducts } = useProduct();
 
   return (
     <div className="grid grid-cols-1 gap-[19px] tablet:grid-cols-4">
@@ -37,7 +19,6 @@ function ProductItemList({ sortOption }: ProductItemListProps) {
         </div>
       ))}
     </div>
-
   );
 }
 
