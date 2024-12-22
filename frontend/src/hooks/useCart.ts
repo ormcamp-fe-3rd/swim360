53928cac4b7951fb6d588611730df1b1209fd1ca
MH <dico659@gmail.com>
2024-12-23 00:20:40 +0900

    fix: 장바구니 업데이트 시점 수정 및 에러 처리

--- frontend/src/hooks/useCart.ts
+++ frontend/src/hooks/useCart.ts
@@ -16,8 +16,12 @@ function useCart() {
 
   const [cartListData, setCartListData] = useState<CartItem[]>([]);
 
+  const [updateCartTrigger, setUpdateCartTrigger] = useState(0);
+
   const handleCartListDataFetch = async () => {
     try {
+      if (!userId) return;
+
       const fetchedCartListData = await getCartListData(userId);
 
       if (!fetchedCartListData) {
@@ -32,7 +36,14 @@ function useCart() {
 
   const handleCartCountFetch = async () => {
     try {
+      if (!userId) return;
+
       const fetchedCartCount = await getCartCount(userId);
+      if (!fetchedCartCount) {
+        setCartCount(0);
+      } else {
+        setCartCount(fetchedCartCount);
+      }
       setCartCount(fetchedCartCount);
     } catch (error) {
       console.log(error);
@@ -43,8 +54,11 @@ function useCart() {
     const prevCount = cartCount;
 
     try {
+      if (!userId) return;
+
       await updateCartData(cartItem);
       setCartCount((prev) => prev + cartItem.quantity);
+      setUpdateCartTrigger((prev) => prev + 1);
     } catch (error) {
       setCartCount(prevCount);
       console.log(error);
@@ -62,7 +76,7 @@ function useCart() {
 
   useEffect(() => {
     handleCartCountFetch();
-  }, []);
+  }, [userId, updateCartTrigger]);
 
   return {
     cartListData,
