a48107e48c05c89f9acee060eccbf51039ba50f4
김혜빈 <hyebinkimdesign@gmail.com>
2024-12-23 16:58:13 +0900

    fix: 리뷰 값이 없을때 예외처리

--- frontend/src/components/product/AverageRating.tsx
+++ frontend/src/components/product/AverageRating.tsx
@@ -6,17 +6,31 @@ interface AverageRatingProp {
 }
 
 export function AverageRating({ reviews }: AverageRatingProp) {
-  let totalRating =
-    reviews?.reduce((sum, review) => sum + review.rating, 0) ?? 0;
-  let averageRating = Math.floor(totalRating / (reviews?.length ?? 0));
+  // 리뷰가 없을 경우 기본값 처리
+  if (!reviews || reviews.length === 0) {
+    return (
+      <div className="flex flex-col items-center">
+        <ReviewStar rating={0} /> {/* 기본 별점 0 */}
+        <div className="flex">
+          <p>0</p>
+          <p>/5</p>
+        </div>
+        <p className="mt-2">작성된 리뷰가 없습니다</p>
+      </div>
+    );
+  }
+
+  // 총합과 평균 계산
+  const totalRating = reviews.reduce((sum, review) => sum + review.rating, 0);
+  const averageRating = Math.round(totalRating / reviews.length);
 
   return (
-    <>
+    <div className="flex flex-col items-center">
       <ReviewStar rating={averageRating} />
       <div className="flex">
         <p>{averageRating}</p>
         <p>/5</p>
       </div>
-    </>
+    </div>
   );
 }
