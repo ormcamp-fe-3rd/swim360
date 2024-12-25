c22fa921c0b4334cc96288eb63d5028a49dc8ec5
MH <dico659@gmail.com>
2024-12-25 13:22:52 +0900

    fix: 장바구니 아이콘의 수량 표시 디자인 수정, seedData 수건 이미지 수정

--- frontend/src/components/home/MainIconButton.tsx
+++ frontend/src/components/home/MainIconButton.tsx
@@ -1,4 +1,9 @@
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@radix-ui/react-tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@radix-ui/react-tooltip";
 import { Link } from "react-router-dom";
 
 import useCart from "@/hooks/useCart";
@@ -24,15 +29,15 @@ export default function MainIconButton() {
       <TooltipProvider delayDuration={200}>
         <Tooltip>
           <TooltipTrigger>
-              <Link to={userId ? "/mypage" : "/login"}>
-                <img
-                  className="hidden h-8 w-8 tablet:block"
-                  src={myPageIcon.imgUrl}
-                  alt={myPageIcon.name}
-                  role="link"
-                  tabIndex={0}
-                />
-              </Link>
+            <Link to={userId ? "/mypage" : "/login"}>
+              <img
+                className="hidden h-8 w-8 tablet:block"
+                src={myPageIcon.imgUrl}
+                alt={myPageIcon.name}
+                role="link"
+                tabIndex={0}
+              />
+            </Link>
           </TooltipTrigger>
           <TooltipContent side="bottom">
             <div className="flex h-10 w-20 items-center justify-center rounded-lg shadow-md">
@@ -45,20 +50,20 @@ export default function MainIconButton() {
       <TooltipProvider delayDuration={200}>
         <Tooltip>
           <TooltipTrigger>
-              <Link to={userId ? "/cart" : "/login"} className="relative">
-                {cartCount > 0 && (
-                  <div className="absolute left-5 top-1 flex h-3 w-3 items-center justify-center rounded-full bg-red-600 text-[8px] text-white">
-                    {cartCount > 99 ? "99" : cartCount}
-                  </div>
-                )}
-                <img
-                  className="h-8 w-8"
-                  src={cartIcon.imgUrl}
-                  alt={cartIcon.name}
-                  role="link"
-                  tabIndex={0}
-                />
-              </Link>
+            <Link to={userId ? "/cart" : "/login"} className="relative">
+              {cartCount > 0 && (
+                <div className="absolute left-[5px] top-1 flex h-3 w-3 items-center justify-center rounded-full bg-red-600 text-[8px] text-white">
+                  {cartCount > 99 ? "99" : cartCount}
+                </div>
+              )}
+              <img
+                className="h-8 w-8"
+                src={cartIcon.imgUrl}
+                alt={cartIcon.name}
+                role="link"
+                tabIndex={0}
+              />
+            </Link>
           </TooltipTrigger>
           <TooltipContent side="bottom">
             <div className="flex h-10 w-20 items-center justify-center rounded-lg shadow-md">
