54e733c30b0adb22f8ea1ef778d5e6fee14ce5e0
MH <dico659@gmail.com>
2024-12-26 20:32:04 +0900

    fix: 포인트 value 삭제제
    Co-authored-by: hyebin kim <hyebinkimsdf@users.noreply.github.com>
    Co-authored-by: kimjigyun <kimjigyun@users.noreply.github.com>
    Co-authored-by: baaanjy <baaanjy@users.noreply.github.com>

--- frontend/src/components/order/PossessionPoint.tsx
+++ frontend/src/components/order/PossessionPoint.tsx
@@ -14,7 +14,6 @@ function PossessionPoint() {
         <div className="flex w-full">
           <input
             type="number"
-            value={0}
             max={0}
             className="mr-5 w-3/4 rounded-sm bg-slate-100 p-2.5"
           />
