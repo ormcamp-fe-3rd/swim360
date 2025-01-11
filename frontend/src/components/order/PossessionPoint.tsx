fcf81a5de9926a6833989acbcd4f580375089fa3
ban <summer.jyyy@gmail.com>
2025-01-11 16:30:49 +0900

    fix: point 입력안되도록 수정

--- frontend/src/components/order/PossessionPoint.tsx
+++ frontend/src/components/order/PossessionPoint.tsx
@@ -1,6 +1,25 @@
+import { useState } from "react";
+
 import PrimaryButton from "@/components/common/PrimaryButton";
 
 function PossessionPoint() {
+  const [inputValue, setInputValue] = useState(0); // 입력값 상태 관리
+  const maxPoints = 0;
+  const minPoints = 0;
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.target.value);
+
+    // min, max 범위 내로 값 제한
+    if (value >= minPoints && value <= maxPoints) {
+      setInputValue(value);
+    } else if (value < minPoints) {
+      setInputValue(minPoints);
+    } else if (value > maxPoints) {
+      setInputValue(maxPoints);
+    }
+  };
+
   return (
     <div>
       <p className="w-full border-b border-black p-2.5 font-bold">포인트</p>
@@ -14,8 +33,10 @@ function PossessionPoint() {
         <div className="flex w-full">
           <input
             type="number"
-            max={0}
-            min={0}
+            value={inputValue}
+            onChange={handleChange}
+            max={maxPoints}
+            min={minPoints}
             className="mr-5 w-3/4 rounded-sm bg-[#e8f0fe] p-2.5"
           />
           <div className="w-1/4">
